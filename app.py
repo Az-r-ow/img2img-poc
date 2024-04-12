c98160fb69c4b5a22e6e2fc36cc272aaf41b4579
Az-r-ow <antoine.azar123@gmail.com>
2024-04-12 13:01:11 +0200

    Added Differential Stable Diffusion Pipeline example

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import math
-import random 
-import torch 
+import random
+import torch
 import gradio as gr
 from PIL import Image, ImageOps
 from diffusers import StableDiffusionInstructPix2PixPipeline
@@ -11,150 +11,190 @@ model_id = "timbrooks/instruct-pix2pix"
 
 def main():
 
-  pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None).to("cuda")
-  example_image = Image.open("imgs/office.jpg").convert("RGB")
+    pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
+        model_id, torch_dtype=torch.float16, safety_checker=None
+    ).to("cuda")
+    example_image = Image.open("imgs/office.jpg").convert("RGB")
 
-  def load_example(
-    steps: int,
-    randomize_seed: bool,
-    seed: int,
-    randomize_cfg: bool,
-    text_cfg_scale: float,
-    image_cfg_scale: float,
-):
-    example_instruction = "Pain the wall in blue"
-    return [example_image, example_instruction] + generate(
-        example_image,
-        example_instruction,
-        steps,
-        randomize_seed,
-        seed,
-        randomize_cfg,
-        text_cfg_scale,
-        image_cfg_scale,
-    )
-
-  def generate(
-      input_image: Image.Image,
-      instruction: str,
-      color: str, 
-      steps: int,
-      randomize_seed: bool,
-      seed: int,
-      randomize_cfg: bool,
-      text_cfg_scale: float,
-      image_cfg_scale: float
-  ):
-    seed = random.randint(0, 100000) if randomize_seed else seed
-    text_cfg_scale = round(random.uniform(6.0, 9.0), ndigits=2) if randomize_cfg else text_cfg_scale
-    image_cfg_scale = round(random.uniform(1.2, 1.8), ndigits=2) if randomize_cfg else image_cfg_scale
-
-    width, height = input_image.size
-    # width, height = (100, 100)
-    factor = 512 / max(width, height)
-    factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
-    width = int((width * factor) // 64) * 64
-    height = int((height * factor) // 64) * 64
-    input_image = ImageOps.fit(input_image, (width, height), method=Image.Resampling.LANCZOS)
-
-    if instruction == "":
-        return [input_image, seed]
-    
-    color_placeholder = "{color}"
-
-    if color_placeholder in instruction:
-      try:
-        color = hex_to_name(color)
-      except ValueError as e:
-          print(e)
-      instruction = instruction.replace(color_placeholder, color)
-
-    generator = torch.manual_seed(seed)
-    edited_image = pipe(
-        instruction, image=input_image,
-        guidance_scale=text_cfg_scale, image_guidance_scale=image_cfg_scale,
-        num_inference_steps=steps, generator=generator,
-    ).images[0]
-
-    return [seed, text_cfg_scale, image_cfg_scale, edited_image]
-  
-  def reset():
-     return [0, "Randomize Seed", 1242, "Fix CFG", 7.5, 1.5, None]
-  
-  with gr.Blocks() as demo:
-    with gr.Row():
-      with gr.Column(scale=1, min_width=100):
-          generate_button = gr.Button("Generate")
-      with gr.Column(scale=1, min_width=100):
-          load_button = gr.Button("Load Example")
-      with gr.Column(scale=1, min_width=100):
-          reset_button = gr.Button("Reset")
-      with gr.Column(scale=2):
-          instruction = gr.Textbox(lines=1, label="Edit Instruction", interactive=True)
-      with gr.Column(scale=1):
-          color = gr.ColorPicker(label="Pick a color", interactive=True)
-
-    with gr.Row():
-      input_image = gr.Image(label="Input Image", type="pil", width=512, height=512, interactive=True)
-      edited_image = gr.Image(label="Edited Image", type="pil",width=512, height=512, interactive=False)
-
-    with gr.Row():
-      steps = gr.Number(value=50, precision=0, label="Steps", interactive=True)
-      randomize_seed = gr.Radio(
-          ["Fix Seed", "Randomize Seed"],
-          value="Randomize Seed",
-          type="index",
-          show_label=False,
-          interactive=True,
-      )
-      seed = gr.Number(value=1371, precision=0, label="Seed", interactive=True)
-      randomize_cfg = gr.Radio(
-          ["Fix CFG", "Randomize CFG"],
-          value="Fix CFG",
-          type="index",
-          show_label=False,
-          interactive=True,
-      )
-      text_cfg_scale = gr.Number(value=7.5, label="Text CFG", interactive=True)
-      image_cfg_scale = gr.Number(value=1.5, label="Image CFG", interactive=True)
-
-    load_button.click(
-        fn=load_example,
-        inputs=[
+    def load_example(
+        steps: int,
+        randomize_seed: bool,
+        seed: int,
+        randomize_cfg: bool,
+        text_cfg_scale: float,
+        image_cfg_scale: float,
+    ):
+        example_instruction = "Pain the wall in blue"
+        return [example_image, example_instruction] + generate(
+            example_image,
+            example_instruction,
             steps,
             randomize_seed,
             seed,
             randomize_cfg,
             text_cfg_scale,
             image_cfg_scale,
-        ],
-        outputs=[input_image, instruction, seed, text_cfg_scale, image_cfg_scale, edited_image],
-    )
-    
-    generate_button.click(
-        fn=generate,
-        inputs=[
-            input_image,
+        )
+
+    def generate(
+        input_image: Image.Image,
+        instruction: str,
+        color: str,
+        steps: int,
+        randomize_seed: bool,
+        seed: int,
+        randomize_cfg: bool,
+        text_cfg_scale: float,
+        image_cfg_scale: float,
+    ):
+        seed = random.randint(0, 100000) if randomize_seed else seed
+        text_cfg_scale = (
+            round(random.uniform(6.0, 9.0), ndigits=2)
+            if randomize_cfg
+            else text_cfg_scale
+        )
+        image_cfg_scale = (
+            round(random.uniform(1.2, 1.8), ndigits=2)
+            if randomize_cfg
+            else image_cfg_scale
+        )
+
+        width, height = input_image.size
+        # width, height = (100, 100)
+        factor = 512 / max(width, height)
+        factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
+        width = int((width * factor) // 64) * 64
+        height = int((height * factor) // 64) * 64
+        input_image = ImageOps.fit(
+            input_image, (width, height), method=Image.Resampling.LANCZOS
+        )
+
+        if instruction == "":
+            return [input_image, seed]
+
+        color_placeholder = "{color}"
+
+        if color_placeholder in instruction:
+            try:
+                color = hex_to_name(color)
+            except ValueError as e:
+                print(e)
+            instruction = instruction.replace(color_placeholder, color)
+
+        generator = torch.manual_seed(seed)
+        edited_image = pipe(
             instruction,
-            color,
-            steps,
-            randomize_seed,
-            seed,
-            randomize_cfg,
-            text_cfg_scale,
-            image_cfg_scale,
-        ],
-        outputs=[seed, text_cfg_scale, image_cfg_scale, edited_image],
-    )
+            image=input_image,
+            guidance_scale=text_cfg_scale,
+            image_guidance_scale=image_cfg_scale,
+            num_inference_steps=steps,
+            generator=generator,
+        ).images[0]
 
-    reset_button.click(
-        fn=reset, 
-        inputs=[],
-        outputs=[steps, randomize_seed, seed, randomize_cfg, text_cfg_scale, image_cfg_scale, edited_image],
-    )
+        return [seed, text_cfg_scale, image_cfg_scale, edited_image]
 
-    demo.queue()
-    demo.launch(share=False, server_name="0.0.0.0", server_port=9000)
+    def reset():
+        return [0, "Randomize Seed", 1242, "Fix CFG", 7.5, 1.5, None]
+
+    with gr.Blocks() as demo:
+        with gr.Row():
+            with gr.Column(scale=1, min_width=100):
+                generate_button = gr.Button("Generate")
+            with gr.Column(scale=1, min_width=100):
+                load_button = gr.Button("Load Example")
+            with gr.Column(scale=1, min_width=100):
+                reset_button = gr.Button("Reset")
+            with gr.Column(scale=2):
+                instruction = gr.Textbox(
+                    lines=1, label="Edit Instruction", interactive=True
+                )
+            with gr.Column(scale=1):
+                color = gr.ColorPicker(label="Pick a color", interactive=True)
+
+        with gr.Row():
+            input_image = gr.Image(
+                label="Input Image", type="pil", width=512, height=512, interactive=True
+            )
+            edited_image = gr.Image(
+                label="Edited Image",
+                type="pil",
+                width=512,
+                height=512,
+                interactive=False,
+            )
+
+        with gr.Row():
+            steps = gr.Number(value=50, precision=0, label="Steps", interactive=True)
+            randomize_seed = gr.Radio(
+                ["Fix Seed", "Randomize Seed"],
+                value="Randomize Seed",
+                type="index",
+                show_label=False,
+                interactive=True,
+            )
+            seed = gr.Number(value=1371, precision=0, label="Seed", interactive=True)
+            randomize_cfg = gr.Radio(
+                ["Fix CFG", "Randomize CFG"],
+                value="Fix CFG",
+                type="index",
+                show_label=False,
+                interactive=True,
+            )
+            text_cfg_scale = gr.Number(value=7.5, label="Text CFG", interactive=True)
+            image_cfg_scale = gr.Number(value=1.5, label="Image CFG", interactive=True)
+
+        load_button.click(
+            fn=load_example,
+            inputs=[
+                steps,
+                randomize_seed,
+                seed,
+                randomize_cfg,
+                text_cfg_scale,
+                image_cfg_scale,
+            ],
+            outputs=[
+                input_image,
+                instruction,
+                seed,
+                text_cfg_scale,
+                image_cfg_scale,
+                edited_image,
+            ],
+        )
+
+        generate_button.click(
+            fn=generate,
+            inputs=[
+                input_image,
+                instruction,
+                color,
+                steps,
+                randomize_seed,
+                seed,
+                randomize_cfg,
+                text_cfg_scale,
+                image_cfg_scale,
+            ],
+            outputs=[seed, text_cfg_scale, image_cfg_scale, edited_image],
+        )
+
+        reset_button.click(
+            fn=reset,
+            inputs=[],
+            outputs=[
+                steps,
+                randomize_seed,
+                seed,
+                randomize_cfg,
+                text_cfg_scale,
+                image_cfg_scale,
+                edited_image,
+            ],
+        )
+
+        demo.queue()
+        demo.launch(share=False, server_name="0.0.0.0", server_port=9000)
 
 
 if __name__ == "__main__":
---
+++ app2.py
@@ -0,0 +1,126 @@
+import gradio as gr
+import spaces
+import torch
+from torchvision import transforms
+from SD2.diff_pipe import StableDiffusionDiffImg2ImgPipeline
+from diffusers import DPMSolverMultistepScheduler
+
+NUM_INFERENCE_STEPS = 50
+device = "cuda"
+
+base = StableDiffusionDiffImg2ImgPipeline.from_pretrained(
+    "timbrooks/instruct-pix2pix",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True,
+).to(device)
+
+
+base.scheduler = DPMSolverMultistepScheduler.from_config(base.scheduler.config)
+# refiner.scheduler = DPMSolverMultistepScheduler.from_config(base.scheduler.config)
+
+
+def preprocess_image(image):
+    image = image.convert("RGB")
+    image = transforms.CenterCrop((image.size[1] // 64 * 64, image.size[0] // 64 * 64))(
+        image
+    )
+    image = transforms.ToTensor()(image)
+    image = image * 2 - 1
+    image = image.unsqueeze(0).to(device)
+    return image
+
+
+def preprocess_map(map):
+    map = map.convert("L")
+    map = transforms.CenterCrop((map.size[1] // 64 * 64, map.size[0] // 64 * 64))(map)
+    # convert to tensor
+    map = transforms.ToTensor()(map)
+    map = map.to(device)
+    return map
+
+
+def inference(image, map, gs, prompt, negative_prompt):
+    validate_inputs(image, map)
+    image = preprocess_image(image)
+    map = preprocess_map(map)
+    edited_images = base(
+        prompt=prompt,
+        original_image=image,
+        image=image,
+        strength=1,
+        guidance_scale=gs,
+        num_images_per_prompt=1,
+        negative_prompt=negative_prompt,
+        map=map,
+        num_inference_steps=NUM_INFERENCE_STEPS,
+        denoising_end=0.8,
+        output_type="latent",
+    ).images[0]
+    # edited_images = refiner(
+    #     prompt=prompt,
+    #     original_image=image,
+    #     image=edited_images,
+    #     strength=1,
+    #     guidance_scale=7.5,
+    #     num_images_per_prompt=1,
+    #     negative_prompt=negative_prompt,
+    #     map=map,
+    #     num_inference_steps=NUM_INFERENCE_STEPS,
+    #     denoising_start=0.8,
+    # ).images[0]
+    return edited_images
+
+
+def validate_inputs(image, map):
+    if image is None:
+        raise gr.Error("Missing image")
+    if map is None:
+        raise gr.Error("Missing map")
+
+
+example1 = [
+    "assets/input2.jpg",
+    "assets/map2.jpg",
+    17.5,
+    "Tree of life under the sea, ethereal, glittering, lens flares, cinematic lighting, artwork by Anna Dittmann & Carne Griffiths, 8k, unreal engine 5, hightly detailed, intricate detailed",
+    "bad anatomy, poorly drawn face, out of frame, gibberish, lowres, duplicate, morbid, darkness, maniacal, creepy, fused, blurry background, crosseyed, extra limbs, mutilated, dehydrated, surprised, poor quality, uneven, off-centered, bird illustration, painting, cartoons",
+]
+example2 = [
+    "assets/input3.jpg",
+    "assets/map4.png",
+    21,
+    "overgrown atrium, nature, ancient black marble columns and terracotta tile floors, waterfall, ultra-high quality, octane render, corona render, UHD, 64k",
+    "Two bodies, Two heads, doll, extra nipples, bad anatomy, blurry, fuzzy, extra arms, extra fingers, poorly drawn hands, disfigured, tiling, deformed, mutated, out of frame, cloned face, watermark, text, lowres, disfigured, ostentatious, ugly, oversaturated, grain, low resolution, blurry, bad anatomy, poorly drawn face, mutant, mutated,  blurred, out of focus, long neck, long body, ugly, disgusting, bad drawing, childish",
+]
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            with gr.Row():
+                input_image = gr.Image(label="Input Image", type="pil")
+                change_map = gr.Image(label="Change Map", type="pil")
+            gs = gr.Slider(0, 28, value=7.5, label="Guidance Scale")
+            prompt = gr.Textbox(label="Prompt")
+            neg_prompt = gr.Textbox(label="Negative Prompt")
+            with gr.Row():
+                clr_btn = gr.ClearButton(
+                    components=[input_image, change_map, gs, prompt, neg_prompt]
+                )
+                run_btn = gr.Button("Run", variant="primary")
+
+        output = gr.Image(label="Output Image")
+    gr.Examples(
+        examples=[example1, example2],
+        inputs=[input_image, change_map, gs, prompt, neg_prompt],
+    )
+    gr.Markdown(
+        "Differential Diffusion with SDXL; Thanks to the community for the prompts in the examples."
+    )
+    run_btn.click(
+        inference,
+        inputs=[input_image, change_map, gs, prompt, neg_prompt],
+        outputs=output,
+    )
+    clr_btn.add(output)
+if __name__ == "__main__":
+    demo.launch()
